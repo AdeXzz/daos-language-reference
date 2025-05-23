fb8aea785c6b5caab2742b3a272f2b38ede2af59
AdeXzz <sethyunior@gmail.com>
2025-05-23 10:42:30 -0500

    Delete frontend/efficiency-record.entity.spec.ts

--- frontend/efficiency-record.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EfficiencyRecordEntity } from './efficiency-record.entity';
-
-describe('EfficiencyRecordEntity', () => {
-  it('should be defined', () => {
-    expect(new EfficiencyRecordEntity()).toBeDefined();
-  });
-});
