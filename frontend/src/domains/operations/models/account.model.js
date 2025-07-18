f03585c00d5bf00b889d20a49b4c7c0e1155d4de
AdeXzz <sethyunior@gmail.com>
2025-07-17 23:06:04 -0500

    Delete frontend directory

--- frontend/src/domains/operations/models/account.model.js
+++
@@ -1,9 +0,0 @@
-export class Account {
-    constructor(id, accountNumber, accountType, balance, createdAt) {
-        this.id = id
-        this.accountNumber = accountNumber
-        this.accountType = accountType
-        this.balance = balance
-        this.createdAt = createdAt
-    }
-}
--- frontend/src/domains/operations/services/account.service.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const BASE = 'http://localhost:3000/accounts'
-
-export default {
-    async fetchAll() {
-        const res = await axios.get(BASE)
-        return res.data
-    }
-}
